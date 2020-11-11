6c75c5bb3c9ba56e651017486811c5aee2316ffe
Collins1738 <tobechikeluba@gmail.com>
2020-11-11 12:55:43 -0500

    fixed initialize user

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -32,14 +32,15 @@ const initializeUser = (req, res) => {
 	const { userId, displayName } = req.body;
 	const doc = database.collection("users").doc(userId);
 	if (!doc.exists) {
-		database.collection("users").add({
+		doc.set({
 			challengesMade: [],
 			challengesPlayed: [],
 			displayName,
-			highscoreSinglePlayer,
-			numSinglePlayerGamesPlayed,
-		});
-		res.json({ message: "Successfully initialized user" });
+			highscoreSinglePlayer: 0,
+			numSinglePlayerGamesPlayed: 0,
+		})
+			.then(() => res.json({ message: "Successfully initialized user" }))
+			.catch((err) => res.json({ error: err.message }));
 	} else {
 		res.json({ message: "User already exists" });
 	}
